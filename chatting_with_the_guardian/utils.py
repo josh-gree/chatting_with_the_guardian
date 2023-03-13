1701f9f70b1788cd9206f466b6e7c6c12c35290d
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2023-03-13 21:46:40 +0000

    add utils and tests

---
+++ chatting_with_the_guardian/utils.py
@@ -0,0 +1,63 @@
+import bs4
+import requests
+from datetime import datetime
+from urllib.parse import urlparse
+
+ROOT_URL = "https://www.theguardian.com"
+
+
+def parse_url(url: str) -> dict:
+    parsed_url = urlparse(url)
+    try:
+        slug = parsed_url.path.split("/")[5]
+        category = parsed_url.path.split("/")[1]
+        date_part = parsed_url.path.split("/")[2:5]
+        date = datetime.strptime("-".join(date_part), "%Y-%b-%d").date()
+    except (IndexError, ValueError):
+        raise ValueError(f"Could not parse URL: {url}")
+
+    return {"slug": slug, "category": category, "date": date}
+
+
+def is_article_url(href: str, root_url: str) -> bool:
+    starts_with_root = href.startswith(root_url)
+
+    parsed_url = urlparse(href)
+
+    try:
+        date_part = parsed_url.path.split("/")[2:5]
+        date_part = datetime.strptime("-".join(date_part), "%Y-%b-%d").date()
+
+        has_date = True
+
+    except (IndexError, ValueError):
+        has_date = False
+
+    return starts_with_root and has_date
+
+
+def get_front_page() -> dict:
+    resp = requests.get(ROOT_URL + "/uk")
+    soup = bs4.BeautifulSoup(resp.content, features="html.parser")
+
+    article_urls = [
+        article.attrs["href"]
+        for article in soup.find_all("a", href=True)
+        if is_article_url(article.attrs["href"], ROOT_URL)
+    ]
+
+    return article_urls
+
+
+def get_article_text(url: str) -> str:
+    resp = requests.get(url)
+    soup = bs4.BeautifulSoup(resp.content)
+
+    ps = [
+        p.text
+        for p in soup.find("div", {"id": "maincontent"}).div.find_all(
+            "p", recursive=False
+        )
+    ]
+
+    return "\n".join(ps)
---
+++ tests/test_utils.py
@@ -0,0 +1,81 @@
+import pytest
+
+from datetime import date
+from chatting_with_the_guardian.utils import (
+    get_front_page,
+    is_article_url,
+    parse_url,
+    get_article_text,
+)
+
+
+def test_parse_url():
+    url = "https://example.com/world/2023/mar/11/ukraine-crisis-russia-troops-border-zelenskiy"
+    expected_output = {
+        "slug": "ukraine-crisis-russia-troops-border-zelenskiy",
+        "category": "world",
+        "date": date(2023, 3, 11),
+    }
+    assert parse_url(url) == expected_output
+
+    # Test an invalid URL that should raise an exception
+    with pytest.raises(ValueError):
+        parse_url("https://example.com/2021-June-30/news/headlines")
+
+
+def test_is_article_url():
+    # Test for a valid article URL with a date
+    href = "https://www.theguardian.com/world/2023/mar/11/ukraine-crisis-russia-troops-border-zelenskiy"
+    root_url = "https://www.theguardian.com"
+    assert is_article_url(href, root_url) == True
+
+    # Test for an article URL without a date
+    href = "https://www.theguardian.com/world/ukraine-crisis-russia-troops-border-zelenskiy"
+    root_url = "https://www.theguardian.com"
+    assert is_article_url(href, root_url) == False
+
+    # Test for a URL from a different domain
+    href = "https://www.example.com/article/2023/mar/11/test-article"
+    root_url = "https://www.theguardian.com"
+    assert is_article_url(href, root_url) == False
+
+
+class MockResponse:
+    def __init__(self, content):
+        self.content = content
+        self.status_code = 200
+        self.headers = {"Content-Type": "text/html"}
+
+    def text(self):
+        return self.content
+
+
+def test_get_front_page(mocker):
+    with open("./tests/html/the_guardian_frontpage.html", "r") as f:
+        html = f.read()
+        mocker.patch("requests.get", return_value=MockResponse(html))
+
+    # Test that the front page returns a list of article URLs
+    article_urls = get_front_page()
+    assert isinstance(article_urls, list)
+    assert len(article_urls) > 0
+
+    # Test that each URL is a valid article URL
+    root_url = "https://www.theguardian.com"
+    for url in article_urls:
+        assert is_article_url(url, root_url) == True
+
+
+def test_get_article_text(mocker):
+    with open("./tests/html/the_guardian_article.html", "r") as f:
+        html = f.read()
+        mocker.patch("requests.get", return_value=MockResponse(html))
+
+    article = get_article_text(
+        "https://www.theguardian.com/politics/2023/mar/12/home-office-removed-image-huw-edwards-tweet-migration-bill-bbc-complaints"
+    )
+
+    assert article.split("\n")[0] == "This is a fake article"
+    assert article.split("\n")[1] == "It Only has two paragraphs"
+
+    assert len(article.split("\n")) == 2
