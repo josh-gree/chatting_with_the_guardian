2ba726fb6938c5ade9658614b7fb95dae1689eb0
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2023-03-19 15:23:02 +0000

    add new job

---
+++ chatting_with_the_guardian/paragraph_embedding_population/main.py
@@ -0,0 +1,35 @@
+import torch
+
+from chatting_with_the_guardian.database import ScopedSession
+from chatting_with_the_guardian.models.article import ArticleParagraph
+
+from transformers import DistilBertTokenizer, DistilBertModel
+
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+
+
+def get_bert_embedding(text, model, tokenizer):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    cls_embedding = outputs.last_hidden_state[:, 0, :]
+    return cls_embedding
+
+
+def main():
+    session = ScopedSession()
+
+    ps = session.query(ArticleParagraph).all()
+
+    p = ps[0]
+
+    p_embed = get_bert_embedding(p.paragraph_text, model, tokenizer)
+
+    print(p_embed)
+
+
+if __name__ == "__main__":
+    main()
