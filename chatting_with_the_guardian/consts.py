78dcdf98aa94fe8d22c2bf3a57f1abd8c0e0286e
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2023-05-08 15:47:12 +0100

    Add consts file

---
+++ chatting_with_the_guardian/consts.py
@@ -0,0 +1,8 @@
+import os
+
+ACCESS_ID = os.environ.get("ACCESS_ID")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DO_SPACES_ENDPOINT_URL = (
+    "https://chatting-with-the-guardian.ams3.digitaloceanspaces.com"
+)
